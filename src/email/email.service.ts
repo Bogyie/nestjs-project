e766c4f6715167dd84fb8b8ad01c2b9ec97168d9
bogyeong <boggyhint@gmail.com>
2022-04-14 16:29:11 +0900

    editing email service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import Mail = require('nodemailer/lib/mailer')
+import * as nodemailer from 'nodemailer';
+
+
+interface EmailOptions {
+  to: string;
+  subject: string;
+  html: string;
+}
 
 @Injectable()
 export class EmailService {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: '',
+      auth: {
+        user: '',
+        pass: ''
+      }
+    });
+  }
+
   async sendUserRegistrationEmail(email: string, signupVerifyToken: string) {
 
   }
